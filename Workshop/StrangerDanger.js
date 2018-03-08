6340def6ecfb2338adeaf896f37896bec3c630b3
[Mmarksp] <[mark@tomani.dk]>
2018-03-08 10:34:19 +0100

    workshop strangerdanger

---
+++ Workshop/StrangerDanger.js
@@ -0,0 +1,37 @@
+var obj = [];
+var bodyCount;
+var i = 0;
+
+class Objects {
+  Constructor(pos, pos, sz) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.sz = sz;
+  }
+  Rotate() {
+    angleMode(DEGREES);
+    rotate(360 / i++);
+  }
+  rect(this.xpos, this.ypos, this.sz)
+}
+
+function setup() {
+  createCanvas(500, 500);
+  obj[i] = new Objects(CENTER, CENTER, HUMAN_SIZE)
+  obj[i].rect();
+}
+
+function draw() {
+  for(i = 0; i < obj.length; i++) {
+    obj[i].rect();
+    obj[i].rotate();
+    delay(100);
+    Objects.push();
+    Draw 1 new Objects in new position relative to coordinates(CENTER, CENTER)
+  }
+
+  if (Touch one another) {
+    let speed = 3
+    let civil inattention = Disperse in random(0, 360) direction with speed value
+  }
+}
