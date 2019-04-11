10a4888c8d6d9de9c901bda6f1364124e2ba0906
[Mmarksp] <[mark@tomani.dk]>
2019-04-11 22:49:10 +0200

    photobooth-program

--- Interaktionsdesign 2/Code/empty-example/SnapBooth2.js
+++ Interaktionsdesign 2/Code/empty-example/SnapBooth2.js
@@ -1,44 +1,53 @@
 var loaded;
 var Img = [];
-var x = 0;
-var y = 0;
+var xPs = 0;
+var yPs = 0;
 var c;
+var counter = 0;
 
 function setup() {
-  c = createCanvas(windowWidth,windowHeight);
-  takeSnap(0);
+  frameRate(1);
+  c = createCanvas(windowWidth, windowHeight);
+  takeSnap(counter);
 }
 
 function takeSnap(i) {
-  saveCanvas(c, 'prototype('+i+')','jpg');
-  loaded = loadImage('prototype('+i+').jpg');
+  // saveCanvas(c, 'prototype('+i+')','jpg');
+  loaded = loadImage('Users/Mark/Downloads/prototype('+i+').jpg');
+  // loaded = loadImage('puffin.jpg');
 
   Img.push(new Imgs(loaded));
 }
 
 function draw() {
-  Img[0].display(windowWidth, windowHeight)
-  // x = windowWidth/5
-  // y = windowHeight/4
-  // for(let xPs = 0; xPs < width; xPs += x) {
-  //   for(let yPs = 0; yPs < height; yPs += y) {  //A nested loop determining where to put each image of the capture feed.
-  //     for(let i = 0; i < Img.length; i++) { //This is to progress the array
-  //       Img[i].display(xPs, yPs, i);
-  //     }
-  //   }
-  // }
+  // Img[0].display(0, 0)
+  let x = windowWidth/5
+  let y = windowHeight/4
+  for(let i = 0; i < Img.length; i++) {
+    Img[i].display(xPs, yPs, x, y);
+  }
+  if (xPs > width) {
+    xPs = 0;
+    yPs += y;
+  } else {
+    xPs += x
+  }
+
+  counter++
+  takeSnap(counter)
 }
+
 
 class Imgs {
   constructor(loaded) {
     this.loaded = loaded;
   }
 
-  display(xPs, yPs) {
-    // this.xPs = xPs;
-    // this.yPs = yPs;
-    // this.x = windowWidth/5;
-    // this.y = windowHeight/4;
+  display(xPs, yPs, x, y) {
+    this.xPs = xPs;
+    this.yPs = yPs;
+    this.x = x;
+    this.y = y;
 
     image(loaded, this.xPs, this.yPs, this.x, this.y); //Every image drawn will show the capture feed
   }
